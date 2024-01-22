2903496443570257e3e7bf4e5a1b3ffab68a6a7b
devngugi <philemonngugi18@gmail.com>
2024-01-22 18:51:50 +0300

    view all customers

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -20,7 +20,7 @@ class CustomerController extends Controller
     public function index()
     {
 
-        $customers = Customer::where('user_id',Auth::User()->id)->paginate(100);
+        $customers = Customer::all()->paginate(100);
         return view('customers', ['customers' => $customers]);
     }
 
